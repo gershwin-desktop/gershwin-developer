5cda677d8db55f4f1c657e71e78abc2972e0b618
pkgdemon <pkgdemonteam@gmail.com>
2026-04-08 18:25:39 -0500

    Add pkgwrap to build

--- Library/Scripts/Install-System-Domain.sh
+++ Library/Scripts/Install-System-Domain.sh
@@ -182,6 +182,11 @@ $MAKE_CMD CPPFLAGS="-DGNUSTEP_INSTALL_TYPE=SYSTEM" -j"$CPUS" || exit 1
 $MAKE_CMD install
 $MAKE_CMD clean
 
+cd "$REPOS_DIR/gershwin-components/pkgwrap"
+$MAKE_CMD CPPFLAGS="-DGNUSTEP_INSTALL_TYPE=SYSTEM" -j"$CPUS" || exit 1
+$MAKE_CMD install
+$MAKE_CMD clean
+
 cd "$REPOS_DIR/gershwin-components/Display"
 $MAKE_CMD CPPFLAGS="-DGNUSTEP_INSTALL_TYPE=SYSTEM" -j"$CPUS" || exit 1
 $MAKE_CMD install
